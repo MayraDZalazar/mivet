246cffdc6c92382e87eda740ab6251b509014aac
MayraDZalazar <damaris.zalazar.m@gmail.com>
2026-02-08 15:06:20 -0300

    inicializo el frontend con Vite y el backend con Express

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("MiVet API running");
+});
+
+app.listen(4000, () => {
+  console.log("Backend running on port 4000");
+});
